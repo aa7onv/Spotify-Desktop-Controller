ef842a130c3d522660d908960f0ebcdcd0e20755
aa7onv <43218149+aa7onv@users.noreply.github.com>
2026-08-17 18:41:26 -0500

    Add secrets.h for WiFi and API credentials

---
+++ secrets.h
@@ -0,0 +1,7 @@
+constexpr const char* WIFI_SSID = "wifi";
+constexpr const char* PASSWORD = "password";
+
+constexpr const char* CLIENT_ID = "client-id";
+constexpr const char* CLIENT_SECRET = "client-secret";
+
+constexpr const char* REFRESH_TOKEN = "refresh-token";
